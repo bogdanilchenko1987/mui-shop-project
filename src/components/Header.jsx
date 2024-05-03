30d69e71ba51164b4492a2b4fe4469863a80d383
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2024-05-03 05:10:43 +0300

    instal mui

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { ShoppingBasket } from '@mui/icons-material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+
+function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5" component="span" sx={{ flexGrow: 1 }}>
+          MUI Shop
+        </Typography>
+        <IconButton color="inherit">
+          <ShoppingBasket />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
